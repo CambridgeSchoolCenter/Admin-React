d4555481c1a97cec39731390ac7eaa80f3fc86f7
Muhammad <muhammad_bw_26@mail.ru>
2023-10-27 10:50:06 +0500

    set true URL

--- src/config.js
+++ src/config.js
@@ -1,2 +1,1 @@
-// export const APP_URL='https://tokenbackend-dw7y.onrender.com';   
-export const APP_URL='http://192.168.0.116:5000';
+export const APP_URL='https://tokenbackend-dw7y.onrender.com';   
